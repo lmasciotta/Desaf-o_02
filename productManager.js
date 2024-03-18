9307491ea9323f3a9e5f2264432fc3afdc72a00c
Lucas Masciotta <lmasciotta87@gmail.com>
2024-03-17 22:46:44 -0300

    first commit

--- productManager.js
+++ productManager.js
@@ -1,12 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
 class ProductManager {
     #products;
+    #path;
     static idProduct = 0;
         
     constructor(){
-        this.#products = [];
+        this.#products = this.#leerProductosInFile();
+        this.#path = path.resolve(__dirname, 'data', 'productos.json');
+        if (!this.#products) {
+            this.#products = [];
+            this.#guardarDatos();
+        }
+    }
+
+    #leerProductosInFile(){
+        try{
+            if(fs.existsSync(this.#path)){
+                const data = fs.readFileSync(this.#path, 'utf8');
+                this.#products = JSON.parse(data);
+                console.log('Productos leídos correctamente.');
+            } else {
+                console.log('El archivo de productos no existe.');
+            }
+        } catch (error){
+            console.error('Error al leer productos:', error);
+        }
     }
     
-    addProduct(title, description, price, thumbnail, code, stock) {
+    async #guardarDatos(){
+        try {
+            await fs.writeFile(this.#path, JSON.stringify(this.#products));
+            console.log('Datos guardados correctamente.');
+        } catch (error) {
+            console.error(`Ocurrió un error inesperado al momento de guardar el archivo de productos: ${error}`);
+        }
+    }
+
+
+   addProduct(title, description, price, thumbnail, code, stock) {
          
         if(!title || !description || !price || !thumbnail || !code || !stock)
            return `Todos los elementos mencionados a continuación deben ingresarse para que se pueda añadir el producto: "title, description, price, thumbnail, code, stock"`;
@@ -14,7 +47,6 @@ class ProductManager {
            if (this.#products.some(product => product.code == code)) {
             return `El código ${code} ya se encuentra registrado en otro producto`;
         }
-        
      
         const newProduct ={
             id:ProductManager.idProduct++, 
@@ -27,6 +59,7 @@ class ProductManager {
         };
 
         this.#products.push(newProduct);
+        this.#guardarDatos();
 
         return `Pruducto agregado satisfactoriamente`
     }
@@ -41,6 +74,33 @@ class ProductManager {
             return product;
         else
             return `Not Found el producto ${item}`;
+    }
+
+    updateProductById(id, objup) {
+        let msg = `El producto con id ${id} no existe`;
+        
+        const index = this.#products.findIndex(p=> p.id === id);
+
+        if (index !== -1){
+            const {id, ...rest} = objup;
+            this.#products[index] = {...this.#products[index], ...rest}
+            this.#guardarDatos();
+            msg = 'Producto actualizado!';
+        } else {
+            return `El producto con id ${id} no existe.`;
+        }
+    }
+
+    deleteProduct(id){
+        let msg = `El producto con id ${id} no existe`;
+
+        const index = this.#products.findIndex(p=> p.id === id);
+        if(index !== -1){
+            this.#products = this.#products.filter(p=> p.id !== id);
+            this.#guardarDatos();
+            msg = `Producto Eliminado!`;
+        }
+
     }
 
 }
--- testing.js
+++ testing.js
@@ -1,10 +1,9 @@
-const ProductManager = require ("./productManager");
+const ProductManager = require("./productManager");
 
-const product1 = new ProductManager();
-const product2 = new ProductManager();
 
-console.log (product1.addProduct('Coca-Cola', 'Gaseosa', 2800, 'http1://image1.com', '1', 30))
-console.log (product2.addProduct('Sprite', 'Gaseosa', 2600, 'http2://image2.com', '2', 30))
+const productManager = new ProductManager();
 
-console.log (product1.getProducts(product1))
-console.log (product2.getProducts(product2))
+console.log(productManager.addProduct('Coca-Cola', 'Gaseosa', 2800, 'http1://image1.com', '1', 30));
+console.log(productManager.addProduct('Sprite', 'Gaseosa', 2600, 'http2://image2.com', '2', 30));
+
+console.log(productManager.getProducts());
